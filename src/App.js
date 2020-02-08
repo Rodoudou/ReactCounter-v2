cc55383dc887b81c406fbeca02dafe561bd01ee6
Redouane <48132401+Rodoudou@users.noreply.github.com>
2020-02-08 13:22:00 +0100

    Au clic sur Add Counter, un nouveau composant Counter devra apparaître  limiterez le nombre de compteurs à 3. Le bouton Add Counter devra disparaître. Les compteurs seront bien sûr indépendants.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import Content from "./components/Content";
 import "./App.css";
 
 function App() {
-  const [counters, setCounters] = useState([0]);
-
+  const [counters, setCounters] = useState([]);
+  /*   const [counters2, setCounters2] = useState([0]);
+   */
   return (
     <div className="App">
       <h1>React Counter v2</h1>
+
+      <div>
+        {counters.length < 3 && (
+          <button
+            onClick={() => {
+              console.log("click");
+              const newCounters = [...counters];
+              newCounters.push(0);
+              setCounters(newCounters);
+            }}
+          >
+            Add Counter
+          </button>
+        )}
+      </div>
 
       {counters.map((cunt, index) => {
         return (
--- src/components/Content.js
+++ src/components/Content.js
@@ -17,17 +17,36 @@ export default function Content({ counters, setCounters, index }) {
   const handleClickReset = event => {
     const newCounters = [...counters];
     newCounters[index] = 0;
-
     setCounters(newCounters);
   };
   return (
     <div className="content">
-      <button>Add Counter</button>
+      <hr />
 
       <div className="buttons">
-        <button onClick={handleClickMoins}>-</button>
-        <button>{counters}</button>
-        <button onClick={handleClickPlus}>+</button>
+        {counters[index] !== 0 && (
+          <button
+            style={{ marginRight: 15, height: 30, width: 30 }}
+            onClick={handleClickMoins}
+          >
+            -
+          </button>
+        )}
+        <button style={{ height: 80, width: 80, marginBottom: 30 }}>
+          {/* ############################# pas compris cette partie ! #############################*/}
+          {counters[index]}
+          {/* pourquoi [index] ? */}
+          {/*<=###################################################################################### */}
+        </button>
+
+        {counters[index] < 10 && (
+          <button
+            style={{ marginLeft: 15, height: 30, width: 30 }}
+            onClick={handleClickPlus}
+          >
+            +
+          </button>
+        )}
       </div>
 
       <button onClick={handleClickReset}>RESET</button>
